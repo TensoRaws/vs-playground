47e704285295d1a4a1abd98a5794b146818f8ef6
Tohru <65994850+Tohrusky@users.noreply.github.com>
2024-09-04 14:55:09 +0800

    feat: encode templates (#7)

---
+++ templates/ESRGAN-AniScale2_Compact-2x.py
@@ -0,0 +1,14 @@
+import os
+import vapoursynth as vs
+from vapoursynth import core
+from vsrealesrgan import realesrgan, RealESRGANModel
+
+if os.getenv('FINALRIP_SOURCE'):
+    clip = core.bs.VideoSource(source=os.getenv('FINALRIP_SOURCE'))
+else:
+    clip = core.bs.VideoSource(source="s.mkv")
+
+clip = core.resize.Bicubic(clip=clip, format=vs.RGBH)
+clip = realesrgan(clip=clip, model=RealESRGANModel.AniScale2_Compact_2x)
+clip = core.resize.Bicubic(clip=clip, matrix_s="709", format=vs.YUV420P16)
+clip.set_output()
