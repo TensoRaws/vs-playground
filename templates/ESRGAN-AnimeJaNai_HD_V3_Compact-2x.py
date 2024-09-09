386096bc61fe656d8ae09466c822a9a10dad702f
Tohrusky <65994850+Tohrusky@users.noreply.github.com>
2024-09-09 16:55:29 +0800

    fix: makefile and templates

--- templates/ESRGAN-AnimeJaNai_HD_V3_Compact-2x.py
+++ templates/ESRGAN-AnimeJaNai_HD_V3_Compact-2x.py
@@ -3,12 +3,12 @@ import vapoursynth as vs
 from vapoursynth import core
 from vsrealesrgan import realesrgan, RealESRGANModel
 
-if os.getenv("FINALRIP_SOURCE"):
-    clip = core.bs.VideoSource(source=os.getenv("FINALRIP_SOURCE"))
+if os.getenv('FINALRIP_SOURCE'):
+    clip = core.bs.VideoSource(source=os.getenv('FINALRIP_SOURCE'))
 else:
     clip = core.bs.VideoSource(source="s.mkv")
 
-clip = core.resize.Bicubic(clip=clip, format=vs.RGBH, matrix_in_s="709")
+clip = core.resize.Bicubic(clip=clip, format=vs.RGBH, matrix_in_s='709')
 clip = realesrgan(clip=clip, model=RealESRGANModel.AnimeJaNai_HD_V3_Compact_2x)
 clip = core.resize.Bicubic(clip=clip, matrix_s="709", format=vs.YUV420P16)
 clip.set_output()
