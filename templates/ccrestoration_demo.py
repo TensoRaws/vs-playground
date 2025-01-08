d7bc2c79cce5095c5fc327483bb8e78a2342abac
Tohru <65994850+Tohrusky@users.noreply.github.com>
2025-01-08 05:06:29 +0000

    feat: update demo (#33)

--- templates/ccrestoration_demo.py
+++ templates/ccrestoration_demo.py
@@ -2,10 +2,10 @@ import os
 
 import vapoursynth as vs
 from vapoursynth import core
+
+import mvsfunc as mvf
 from ccrestoration import AutoModel, BaseModelInterface, ConfigType
 
-
-# --- sisr, use fp16 to inference (vs.RGBH)
 
 model: BaseModelInterface = AutoModel.from_pretrained(
     pretrained_model_name=ConfigType.RealESRGAN_AnimeJaNai_HD_V3_Compact_2x, tile=None
@@ -16,27 +16,7 @@ if os.getenv("FINALRIP_SOURCE"):
 else:
     clip = core.bs.VideoSource(source="s.mkv")
 
-clip = core.resize.Bicubic(clip=clip, matrix_in_s="709", format=vs.RGBH)
+clip = mvf.ToRGB(clip, depth=16, sample=vs.FLOAT)
 clip = model.inference_video(clip)
-clip = core.resize.Bicubic(clip=clip, matrix_s="709", format=vs.YUV420P16)
+clip = mvf.ToYUV(clip, matrix="709", css="420", depth=10)
 clip.set_output()
-
-# ---  use fp32 to inference (vs.RGBS)
-
-# model: BaseModelInterface = AutoModel.from_pretrained(
-#     pretrained_model_name=ConfigType.RealESRGAN_AnimeJaNai_HD_V3_Compact_2x,
-#     fp16=False,
-# )
-#
-# clip = core.resize.Bicubic(clip=clip, matrix_in_s="709", format=vs.RGBS)
-
-# --- vsr
-
-# model: BaseModelInterface = AutoModel.from_pretrained(
-#     pretrained_model_name=ConfigType.AnimeSR_v2_4x
-# )
-#
-# clip = core.resize.Bicubic(clip=clip, matrix_in_s="709", format=vs.RGBH)
-# clip = model.inference_video(clip)
-# clip = core.resize.Bicubic(clip=clip, matrix_s="709", format=vs.YUV420P16)
-# clip.set_output()
